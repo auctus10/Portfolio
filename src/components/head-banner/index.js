25a5c942a243b3d8eb5416db9c119e0a63563b1e
auctus10 <atin1996io@gmail.com>
2020-05-27 01:14:17 +0530

    fixed a bit of css

--- src/components/head-banner/index.js
+++ src/components/head-banner/index.js
@@ -10,14 +10,16 @@ const HeadBanner = () => {
    <>
      <div className="head-banner">
         <Image className="profile-image" src={require('../../assets/atin.jpg')} size='medium' circular />
-        <Header as='h1' size="massive" color='orange'>Atin Kumar Singh</Header>
-        <Typist style={{margin: "0.6rem"}} cursor={{ show: false }}>
-          <span style={{color: "#f2711c", fontSize: "1.3rem"}}> Hello, Welcome to my website! </span>
-        </Typist>
-        <List style={{margin: "0.8rem", color: "#e8eaf6", fontSize: "1.3rem"}} celled horizontal>
-            {detailList.map((i) => <List.Item>{i.text}</List.Item>)}
-        </List>
-        <Icon className="pointer-icon" name="angle double down" color="orange" size="large" />
+        <div className="content-container">
+            <Header as='h1' size="massive" color='orange'>Atin Kumar Singh</Header>
+            <Typist style={{margin: "0.6rem"}} cursor={{ show: false }}>
+               <span style={{color: "#f2711c", fontSize: "1.3rem"}}> Hello, Welcome to my website! </span>
+            </Typist>
+            <List style={{margin: "0.8rem", color: "#e8eaf6", fontSize: "1.3rem"}} celled horizontal>
+               {detailList.map((i) => <List.Item>{i.text}</List.Item>)}
+            </List>
+            <Icon className="pointer-icon" name="angle double down" color="orange" size="large" />
+        </div>
      </div>
    </>
    )
