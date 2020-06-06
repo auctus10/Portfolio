664e78b1b320640c86cc507441ab47d48ec9a232
auctus10 <atin1996io@gmail.com>
2020-06-06 18:34:40 +0530

    added about page

---
+++ src/components/about/socialsList.js
@@ -0,0 +1,28 @@
+import githubImg from '../../assets/github-icon.webp';
+import stackOverFlow from '../../assets/stackoverflow-512.png';
+import linkedIn from '../../assets/linkedin.webp';
+import instagramImg from '../../assets/instagram.webp';
+import facebookImg from '../../assets/facebook.png';
+
+export default [
+    {
+        link: 'https://github.com/auctus10',
+        image: githubImg,
+    },
+    {
+        link: 'https://stackoverflow.com/users/10574385/atin-singh',
+        image: stackOverFlow,
+    },
+    {
+        link: 'https://www.linkedin.com/in/atin-singh-b23389134/',
+        image: linkedIn,
+    },
+    {
+        link: 'https://www.instagram.com/atin_auc/',
+        image: instagramImg
+    },
+    {
+        link: 'https://www.facebook.com/atinkumar.singh1/',
+        image: facebookImg
+    }
+]
