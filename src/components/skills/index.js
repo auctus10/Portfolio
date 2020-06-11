b84e8056a24fb5b7d76aa00d3ea0ec052c8c6e17
auctus10 <atin1996io@gmail.com>
2020-06-11 14:47:54 +0530

    removed hardcoded data

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,63 +1,32 @@
 import React from 'react'
-import { Header, Grid, Card } from 'semantic-ui-react';
+import { Header, Grid, Card, Image } from 'semantic-ui-react';
+
+import techSkillsData from './techSkills';
+
+import './index.css';
 
 const TechSkills = () => {
     return(
       <div> 
         {' '}
-
         <Header as='h1' size='massive' color='orange'>Things that I know!</Header>
-        <Grid className="Screen-Container">
-          <Grid.Column style={{width: 'auto'}}>
-            <Grid.Row>
-              <Card
-                href='#card-example-link-card'
-                header='Elliot Baker'
-                meta='Friend'
-                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-              />
-              <Card
-                href='#card-example-link-card'
-                header='Elliot Baker'
-                meta='Friend'
-                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-              />
-            </Grid.Row>
-          </Grid.Column>
-
-          <Grid.Column style={{width: 'auto'}}>
-            <Grid.Row>
-              <Card
-                href='#card-example-link-card'
-                header='Elliot Baker'
-                meta='Friend'
-                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-              />
-              <Card
-                href='#card-example-link-card'
-                header='Elliot Baker'
-                meta='Friend'
-                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-              />
-            </Grid.Row>
-          </Grid.Column>
-
-          <Grid.Column style={{width: 'auto'}}>
-            <Grid.Row>
-              <Card
-                href='#card-example-link-card'
-                header='Elliot Baker'
-                meta='Friend'
-                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-              />
-              <Card
-                href='#card-example-link-card'
-                header='Elliot Baker'
-                meta='Friend'
-                description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-              />
-            </Grid.Row>
-          </Grid.Column>
+        <Grid centered className="Screen-Container tech-container">
+          {techSkillsData.map((item) => (
+            <Card className='skill-cards'>
+              <Card.Content>
+                <Image
+                  size='mini'
+                  src={item.imgUrl}
+                />
+                <Card.Header>{item.text}</Card.Header>
+                <Card.Meta>{item.meta}</Card.Meta>
+                <Card.Description>
+                  {item.description}
+                  {' '}
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          ))}
         </Grid>
       </div>
 )
---
+++ src/components/skills/techSkills.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        text: 'React js',
+        meta: 'Frontend Web Development',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+    },
+    {
+        text: 'Node js',
+        meta: 'Backend Web Development',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/233_Node_Js_logo-512.png'
+    },
+    {
+        text: 'Docker',
+        meta: 'Devops',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+    },
+    {
+        text: 'MongoDB',
+        meta: 'Database Management System',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+    },
+    {
+        text: 'Git',
+        meta: 'Version Control (VCS)',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+    },
+    {
+        text: 'Redux js',
+        meta: 'Frontend State management system',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+    },
+    {
+        text: 'Kubernetees',
+        meta: 'Devops',
+        description: 'Steve wants to add you to the group ',
+        imgUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+    },
+]
