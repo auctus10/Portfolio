e138c946804c58e56043d4de16b54ef08f9d0cf7
auctus10 <atin1996io@gmail.com>
2020-07-24 15:32:43 +0530

    Added a header to contact screen

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Header } from 'semantic-ui-react';
 
 const ContactMe = () => {
     return(
       <>
-        Contact me here!
+        <div className="Screen-Container">
+          <Header as='h1' size='massive' color='orange'>Reach Out to me!</Header>
+          
+        </div>
+       
       </>
     )
 }
