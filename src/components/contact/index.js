3b3598ce005f579299c4073af4c14872c17c5c36
auctus10 <atin1996io@gmail.com>
2020-07-24 16:09:35 +0530

    completed the contact screen

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Container } from 'semantic-ui-react';
+
+import './index.css'
 
 const ContactMe = () => {
     return(
       <>
         <div className="Screen-Container">
-          <Header as='h1' size='massive' color='orange'>Reach Out to me!</Header>
-          
+          <Header as='h1' size='massive' color='orange'>Reach out to me!</Header>
+          <Container text className='text-container'>
+            <p style={{margin: 'auto'}}>Get in touch with me by e-mailing me at:</p>
+            <p><a href="mailto: atin1996io@gmail.com">atin1996io@gmail.com</a></p>
+          </Container>
         </div>
        
       </>
