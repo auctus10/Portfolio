c96ecb60317e73f324ba1a6aaa4ebd6e18823f92
auctus10 <atin1996io@gmail.com>
2020-07-26 16:56:57 +0530

    added github repos

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Header, Grid, Card, Image } from 'semantic-ui-react';
+import axios from 'axios';
+
+import githubImg from '../../assets/github-icon.webp';
 
 const Projects = () => {
+  const [githubRepos, setGithubRepos] = useState([]);
+ 
+  const fetchRepos = async () => {
+    const data = await axios.get('https://api.github.com/users/auctus10/repos');
+
+    setGithubRepos(data.data);
+  }
+ 
+  useEffect(() => { fetchRepos();},[]);
+
     return(
       <>
-        Projects here
+        <div className="Screen-Container">
+          <Header as='h1' size='massive' color='orange'>Projects that I have done!</Header>
+          <Grid>
+            {githubRepos &&  githubRepos.map(item => (
+              <Card key={item.id} target='_blank' rel='noopener noreferrer' href={item.html_url} className='skill-cards'>
+                <Card.Content>
+                  <Image
+                    style={{marginBottom: '1rem'}}
+                    size='mini'
+                    src={githubImg}
+                  />
+                  <Card.Header>{item.name}</Card.Header>
+                  <Card.Meta>{item.full_name}</Card.Meta>
+                  <Card.Description>
+                    {item.description}
+                    {' '}
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+              ))}
+          </Grid>
+        </div>
       </>
     );
 }
